6f6b2af8096139b7cabf439b082c07d4c92f9751
VickyKumarOfficial <vickykofficial890@gmail.com>
2025-09-19 13:50:44 +0530

    Throws Exception

---
+++ ExceptionHandling/ThrowsExep.java
@@ -0,0 +1,13 @@
+public class ThrowsExep {
+        static void fun() throws IllegalAccessException {
+            System.out.println("inside fun(). ");
+            throw new IllegalAccessException("demo");
+        }
+        public static void main(String[] args) {
+            try {
+                fun();
+            } catch(IllegalAccessException e) {
+                System.out.println("caught in main.");
+            }
+        }
+}
